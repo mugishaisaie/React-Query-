d413cdfa8bf375c0d1ff46bb50206462a9bd6b65
mugisha isaie <mugishaisaieog1@gmail.com>
2024-11-27 06:37:09 +0200

    fetching and testing errors with react query

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,8 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import customFetch from './Utils';
+const Items = () => {
+
+  const {isLoading,data,isError,error} = useQuery({
+    queryKey: ['tasks'],
+    queryFn: ()=>customFetch.get('/')
+  })
+  console.log(data)
+        if(isLoading) return <p style={{marginTop:'1rem', fontSize:'1.3rem'}}>Loading....</p>
+        if(isError) return <p style={{marginTop:'1rem', fontSize:'1.3rem'}}>There Was an Error</p>
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>

608388b20e15c80381d938c17fe237e186368483
mugisha isaie <mugishaisaieog1@gmail.com>
2024-11-28 10:59:33 +0200

    using GET,POST,PATCH and DELETE for data on server with Thunder client extension

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,10 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./Utils";
+
 const SingleItem = ({ item }) => {
+
+  const queryClient= useQueryClient();
+
+  const {mutate: editTask} = useMutation({
+    mutationFn: ({taskId,isDone})=>{
+      return customFetch.patch(`/${taskId}`,{isDone})
+    },
+    onSuccess: ()=>{
+      queryClient.invalidateQueries({queryKey:['tasks']})
+    }
+  })
+
+  const {mutate: deleteTask,isLoading} = useMutation({
+    mutationFn: (taskId)=>{
+      return customFetch.delete(`/${taskId}`)
+    },
+    onSuccess: ()=>{
+      queryClient.invalidateQueries({queryKey:['tasks']})
+    }
+
+  })
   return (
     <div className='single-item'>
       <input
         type='checkbox'
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() => editTask({taskId: item.id, isDone: !item.isDone})}
       />
       <p
         style={{
@@ -17,7 +41,8 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        disabled={isLoading}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
